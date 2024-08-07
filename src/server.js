42c6a4cbfc309adefeec60f972abce0867c71910
joabe-nascimento <joabefnascimento1@outlook.com>
2024-08-06 21:53:25 -0300

    ajuste da api

--- src/server.js
+++ src/server.js
@@ -2,29 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const feedbackRoutes = require("./routes/feedbackRoutes");
-const db = require("./db/database"); // Configuração do banco de dados
+const db = require("./db/database"); // Importa a configuração do banco de dados
 
+db();
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
-// Configuração CORS
-const corsOptions = {
-  origin: ["http://localhost:3000", "https://data7-coral.vercel.app"], // Adicione aqui suas origens permitidas
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 
-// Roteamento
 app.use("/api/feedback", feedbackRoutes);
-
-// Tratamento de erros
-app.use((err, req, res, next) => {
-  console.error("Erro no servidor:", err);
-  res.status(500).send("Erro interno do servidor");
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
