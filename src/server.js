2a07a38d22989e1441a1429a52a047dd78e3accf
joabe-nascimento <joabefnascimento1@outlook.com>
2024-08-06 21:57:57 -0300

    ajuste da api

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
+require("dotenv").config(); // Adiciona a configuração das variáveis de ambiente
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const feedbackRoutes = require("./routes/feedbackRoutes");
-const db = require("./db/database"); // Importa a configuração do banco de dados
+const db = require("./db/database"); // Importa o módulo de conexão com o MongoDB
 
-db();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Configura o CORS para permitir todas as origens
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Configura o bodyParser para tratar JSON
 
-app.use("/api/feedback", feedbackRoutes);
+app.use("/api/feedback", feedbackRoutes); // Configura a rota para feedbacks
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
